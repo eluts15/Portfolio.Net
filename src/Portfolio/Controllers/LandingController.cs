cae90cdf03962bd6e66ed40c56245935f67453ca
eluts <eluts15@gmai.com>
2017-08-25 12:39:34 -0700

    display all starred repository repo names!

--- src/Portfolio/Controllers/LandingController.cs
+++ src/Portfolio/Controllers/LandingController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +11,12 @@ namespace Portfolio.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetStarredRepos()
+        {
+            var repoList = StarredRepos.GetStarredRepos();
+            return View(repoList);
         }
     }
 }
---
+++ src/Portfolio/Models/StarredRepos.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class StarredRepos
+    {
+        public string Name { get; set;} // Repository name.
+        public string Description { get; set; } // Displays the Description.
+        public int Stargazer_Count { get; set; } // Displays the number of stars for this particular repo.
+
+
+        public static List<StarredRepos> GetStarredRepos()
+        {
+                RestClient client = new RestClient("https://api.github.com");
+                RestRequest request = new RestRequest("/users/eluts15/starred", Method.GET);
+                client.AddDefaultHeader("User-Agent", "eluts15"); // Github requires this header.
+                RestResponse response = new RestResponse();
+                Task.Run(async () =>
+                {
+                    response = await GetResponseContentAsync(client, request) as RestResponse;
+                }).Wait();
+
+                JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+                var starredRepositories = JsonConvert.DeserializeObject<List<StarredRepos>>(jsonResponse.ToString());
+                return starredRepositories;
+            }
+
+            public static Task<IRestResponse> GetResponseContentAsync(RestClient client, RestRequest request)
+            {
+                var tcs = new TaskCompletionSource<IRestResponse>();
+                client.ExecuteAsync(request, response => {
+                    tcs.SetResult(response);
+                });
+                return tcs.Task;
+            }
+    }
+}
