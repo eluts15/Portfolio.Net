b5311796652983e0478540041e54e66d5bc5a46b
eluts <eluts15@gmai.com>
2017-08-25 14:34:36 -0700

    import materialize and some odd color choices...

---
+++ src/Portfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetStarredRepos()
+        {
+            var repoList = StarredRepos.GetStarredRepos();
+            return View(repoList);
+        }
+    }
+}
