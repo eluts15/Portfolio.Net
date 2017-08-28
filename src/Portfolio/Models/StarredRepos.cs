819fd55fdba52f428c8231f5d57d9b970b4cdda4
eluts <eluts15@gmai.com>
2017-08-28 14:32:50 -0700

    sort in descending order based on the number of stars.(high to low).

--- src/Portfolio/Models/StarredRepos.cs
+++ src/Portfolio/Models/StarredRepos.cs
@@ -8,18 +8,17 @@ namespace Portfolio.Models
 {
     public class StarredRepos
     {
-        public string Name { get; set;} // Repository name.
-        public string Description { get; set; } // Displays the Description.
+        public string Name { get; set;}
+        public string Description { get; set; }
         public string Url { get; set; }
         public string Language { get; set; }
-        public int Stargazers_Count { get; set; } // Displays the number of stars for this particular repo. Not currently working.
-        public int Watchers { get; set; }
-        public int Forks { get; set; }
+        public int Stargazers_Count { get; set; }
+        public int Watchers_Count { get; set; }
 
         public static List<StarredRepos> GetStarredRepos()
         {
                 RestClient client = new RestClient("https://api.github.com");
-                RestRequest request = new RestRequest("search/repositories?q=user:eluts15&order=desc&sort=stars", Method.GET); //This endpoint just lists all the repos under this user. Where are these damn stars at. LF starsssss.
+                RestRequest request = new RestRequest("search/repositories?q=user:eluts15+fork:true&sort=stars&order=desc&page=1&page&per_page=3", Method.GET);
                 client.AddDefaultHeader("User-Agent", "eluts15"); // Github requires this header.
                 RestResponse response = new RestResponse();
                 Task.Run(async () =>
